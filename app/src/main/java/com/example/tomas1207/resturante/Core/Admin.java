ec2704e4a0815c76410b67b912f08cf968638600
tomas1207 <tomasfernandes98@gmail.com>
2018-06-07 15:40:32 +0100

    Main Form For user ready

--- app/src/main/java/com/example/tomas1207/resturante/Core/Admin.java
+++ app/src/main/java/com/example/tomas1207/resturante/Core/Admin.java
@@ -2,14 +2,18 @@ package com.example.tomas1207.resturante.Core;
 
 public class Admin {
     private String Nome;
-    private Boolean AdminsCHeck;
+    private Boolean AdminsCheck;
 
     public   boolean  AdminCheck(Pessoas pessoas){
         if(pessoas.getAdmin() == true ){
-            this.AdminsCHeck = true;
+            this.AdminsCheck = true;
             return true;
         }
         return false;
     }
+    public  void Admin(Pessoas pessoas){
+        this.Nome = pessoas.getNome();
+    }
+
     //TODO:Add admins gestao, como as noticas e add a form de admin, Remover e add admins,Check de sms para os admins
 }
